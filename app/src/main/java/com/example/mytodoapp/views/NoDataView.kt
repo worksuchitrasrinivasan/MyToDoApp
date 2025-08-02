46ff015b8cea31d807921cc9461a3ab7d6a1f89a
Md Nadim Akhtar <nadimakhtar97@gmail.com>
2025-08-03 02:13:34 +0530

    add text.

--- app/src/main/java/com/example/mytodoapp/views/NoDataView.kt
+++ app/src/main/java/com/example/mytodoapp/views/NoDataView.kt
@@ -6,14 +6,18 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import com.example.mytodoapp.R
 
 
@@ -21,7 +25,10 @@ import com.example.mytodoapp.R
 fun NoDataView(innerPadding: PaddingValues) {
 
     Box(modifier = Modifier.padding(innerPadding).fillMaxSize(), contentAlignment = Alignment.Center) {
-        Image(painterResource(R.drawable.logo_no_fill), contentDescription = stringResource(R.string.no_task))
+        Column {
+            Image( modifier = Modifier.size(80.dp), alignment = Alignment.Center ,painter = painterResource(R.drawable.logo_no_fill), contentDescription = stringResource(R.string.no_task))
+            Text(textAlign = TextAlign.Center, text = stringResource(R.string.you_have_no_tasks))
+        }
         Column(modifier = Modifier.padding(innerPadding).fillMaxSize()) {
             Icon(Icons.Filled.Add, "Localized description")
         }
