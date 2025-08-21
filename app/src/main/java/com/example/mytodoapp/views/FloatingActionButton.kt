54ab5e7b460ea966821bb947febc32cae74377d1
Md Nadim Akhtar <nadimakhtar97@gmail.com>
2025-08-22 00:37:52 +0530

    Add ime padding.

--- app/src/main/java/com/example/mytodoapp/views/FloatingActionButton.kt
+++ app/src/main/java/com/example/mytodoapp/views/FloatingActionButton.kt
@@ -1,11 +1,13 @@
 package com.example.mytodoapp.views
 
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Done
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
 import com.example.mytodoapp.R
 
@@ -13,6 +15,7 @@ import com.example.mytodoapp.R
 @Composable
 fun FloatingActionButton(route: Screen, onClick: () -> Unit) {
     FloatingActionButton(
+        modifier = Modifier.imePadding(),
         onClick = onClick,
         containerColor = colorResource(R.color.colorAccent)
     ) {
